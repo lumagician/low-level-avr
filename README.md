de0612c6cf332af597d1c8fc6ab53fd83472589c
Leo Hofer <leo.hofer@hofernet.ch>
2022-05-08 15:27:21 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Low Level AVR Beispiele
+
+## 01: Interrupts
---
+++ examples/01_Interrupts/main.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void DelayOneSecond(void);
+
+int main (void) {
+	DDRB |= (1<<PORTB5);
+
+	// globale Interrupts aktivieren --> GIE = 1
+	sei();
+
+	// endless loop blinks with a frequency of 0.5 Hz
+	while(1)
+	{
+		PORTB = 0xff;
+		DelayOneSecond();
+		PORTB = 0x00;
+		DelayOneSecond();
+		DelayOneSecond();
+	}
+}
+
+void DelayOneSecond(void)
+{
+	// these nested loops take exactly 1 second to execute
+	for(uint16_t i = 0; i < 10000; i++)
+	{
+		for(uint8_t j = 0; j < 158; j++)
+		{
+
+		}
+	}
+}
